81c61ff3b98419b874699aff031255e9b0590f64
zengjiachengcode <2356924146@qq.com>
2021-10-19 20:17:49 +0800

    jwt标准认证已经完成

--- controller/adminContrl/isAdmin.js
+++ controller/adminContrl/isAdmin.js
@@ -1,15 +1,27 @@
 // 存储报修信息
 const dbconfig = require("../../util/dbconfig.js");
+// token
+const jwt = require("jsonwebtoken");
+// 加密规则
+const secret = "access_api_after_login";
 
 // 验证身份中间件
 module.exports = function (req, res, next) {
   if (req.headers.authorization === undefined || req.headers.authorization === null) {
     return res.sendResult(null, 422, "缺少token");
   }
-  let token = req.headers.authorization.split(" ");
-  const mg_id = token[1].split(".")[0];
-  const mg_name = token[1].split(".")[1];
+  // 1.拿到最后一个元素，也就是token
+  let token = req.headers.authorization.split(" ").pop();
+  // 2.判断token是否有效
+  try {
+    let tokenObj = jwt.verify(token, secret);
+  } catch (error) {
+    return res.sendResult(null, 422, "token无效");
+  }
+  // 3.对token进行解密
+  let tokenObj = jwt.verify(token, secret);
 
+  const { mg_id, mg_name } = tokenObj;
   let sql = `select * from rp_manager where mg_id = ${mg_id} and mg_name = '${mg_name}'`;
   let sqlArr = [];
   let callBack = (err, data) => {
--- controller/adminContrl/login.js
+++ controller/adminContrl/login.js
@@ -1,11 +1,13 @@
 // 存储报修信息
 const dbconfig = require("../../util/dbconfig.js");
 // token
-let jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+// 加密规则
+const secret = "access_api_after_login";
 
 function login(req, res, next) {
   const { username, password } = req.body;
-  let sql = `select mg_id,mg_name,mg_pwd from rp_manager where mg_name='${username}' and mg_pwd='${password}'`;
+  let sql = `select * from rp_manager where mg_name='${username}' and mg_pwd='${password}'`;
   let sqlArr = [];
   let callBack = (err, data) => {
     if (err) {
@@ -13,31 +15,14 @@ function login(req, res, next) {
       res.sendResult(null, 500, "系统故障");
     } else {
       if (data.length === 0) return res.sendResult(data, 422, "登录失败，账号或密码错误");
-      const token = data[0].mg_id + "." + data[0].mg_name;
+      // 1.从查询到的数据中拿到id和name
+      const { mg_id, mg_name } = data[0];
+      // 2.返回token，并设置过期时间为1天
+      const token = jwt.sign({ mg_id, mg_name }, secret, { expiresIn: 60 * 60 * 24 });
       res.sendResult({ token }, 200, "管理员登录成功");
     }
   };
   dbconfig.sqlConnect(sql, sqlArr, callBack);
 }
-
-// function verify(req, res, next) {
-//   let token = req.headers.authorization.split(" ");
-//   const mg_id = token[1].split(".")[0];
-//   const mg_name = token[1].split(".")[1];
-
-//   let sql = `select * from rp_manager where mg_id = ${mg_id} and mg_name = '${mg_name}'`;
-//   let sqlArr = [];
-//   let callBack = (err, data) => {
-//     if (err) {
-//       console.log("用户查询失败", err);
-//       res.sendResult(null, 500, "系统故障");
-//     } else {
-//       if (data.length === 0) return res.sendResult(data, 422, "登录失败，token错误");
-//       if (Number(data[0].mg_state) === 0) return res.sendResult(data, 422, "登录失败，当前管理员已被禁用");
-//       res.sendResult(null, 200, "登录成功");
-//     }
-//   };
-//   dbconfig.sqlConnect(sql, sqlArr, callBack);
-// }
 
 module.exports = { login };
