ae3fd202f51bee97ef661d73b1c73737a0471800
zengjiachengcode <2356924146@qq.com>
2021-10-23 09:35:26 +0800

    删除报修记录接口以完成

--- controller/adminContrl/record.js
+++ controller/adminContrl/record.js
@@ -1,8 +1,9 @@
 // 存储报修信息
 const dbconfig = require("../../util/dbconfig.js");
 
+// 获取报修记录
 function record(req, res) {
-  const query = req.query.query || "";
+  const query = req.query.query;
   const currentpage = Number(req.query.currentpage);
   const pagesize = Number(req.query.pagesize);
   if (!/^\d+$/.test(currentpage) || !currentpage >= 1 || !/^\d+$/.test(pagesize)) {
@@ -11,17 +12,20 @@ function record(req, res) {
   // 偏移量
   const offset = (currentpage - 1) * pagesize;
   // 分页查询伪代码：sql： "select * from rp_record limit offset, pagesize";
-  // let sql = "select * from rp_record limit" + offset + "," + pagesize;
-  // let sqlCount = "select count(*) from rp_record";
-  let sql = "select *, count(*) over() as total from rp_record order by u_id desc limit " + offset + "," + pagesize;
-  let sqlArr = [];
-  let callback = (err, data) => {
+  let sql;
+  if (query === "" || query === null) {
+    sql = "select *, count(*) over() as total from rp_record order by u_id desc limit " + offset + "," + pagesize;
+  } else {
+    sql = "select *, count(*) over() as total from rp_record order by u_id desc limit " + offset + "," + pagesize;
+  }
+  const sqlArr = [];
+  const callback = (err, data) => {
     if (err) {
       console.log("报修记录查询失败", err);
       res.sendResult(null, 500, "系统故障");
     } else {
       let rData = {};
-      rData.totalpage = data[0].total;
+      rData.total = data[0].total;
       rData.currentpage = currentpage;
       rData.record = data;
       res.sendResult(rData, 200, "查询成功");
@@ -30,11 +34,27 @@ function record(req, res) {
   dbconfig.sqlConnect(sql, sqlArr, callback);
 }
 
+// 删除报修记录
+function deleteRecord(req, res) {
+  const u_id = req.params.id;
+  let sql = "delete from rp_record where u_id=" + u_id;
+  const sqlArr = [];
+  const callBack = (err, data) => {
+    if (err) {
+      console.log("删除失败", err);
+      res.sendResult(null, 500, "系统故障");
+    } else {
+      res.sendResult(null, 200, "删除成功");
+    }
+  };
+  dbconfig.sqlConnect(sql, sqlArr, callBack);
+}
+
 function searchRecord(req, res) {
   const u_phone = req.query.u_phone;
   let sql = `select * from rp_record where ${u_phone}`;
-  let sqlArr = [];
-  let callBack = (err, data) => {
+  const sqlArr = [];
+  const callBack = (err, data) => {
     if (err) {
       console.log("报修记录查询失败", err);
       res.sendResult(null, 500, "系统故障");
@@ -48,4 +68,5 @@ function searchRecord(req, res) {
 module.exports = {
   searchRecord,
   record,
+  deleteRecord,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,11 @@ var router = express.Router();
 let isAdmin = require("../controller/adminContrl/isAdmin.js");
 // 路由处理函数
 let { login } = require("../controller/adminContrl/login.js");
-let { record } = require("../controller/adminContrl/record.js");
+let { record, deleteRecord } = require("../controller/adminContrl/record.js");
 
 /* GET users listing. */
 router.post("/login", login);
 router.get("/record", isAdmin, record);
+router.delete("/record/:id", isAdmin, deleteRecord);
 
 module.exports = router;
